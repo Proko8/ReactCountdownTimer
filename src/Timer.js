cd6176f1cc3a4ed5f520753beb6faaebfc5b15e0
Paul Prokopy <pprokopy@gmail.com>
2020-07-20 16:27:59 -0400

    20 July first

---
+++ src/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Timer() {
+    return (
+        <div className="timer__top">
+            <input type="number" className="timer__numberInput">
+
+            </input>
+            <button > Start / Pause</button>
+        </div>
+    )
+}
+
+export default Timer
